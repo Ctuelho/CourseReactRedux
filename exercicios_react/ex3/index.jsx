38f1145a6160d1f1a810d19f9d864daedfcb4aec
Túlio Duarte <duartulio@gmail.com>
2018-07-18 13:36:56 -0300

    exporting component with props

---
+++ exercicios_react/ex3/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Component from './component.jsx'
+
+ReactDOM.render(<Component value="Hello react!"/>, document.getElementById('app'))
+
--- exercicios_react/webpack.config.js
+++ exercicios_react/webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack')
 
 module.exports = {
-    entry: './ex2/index.jsx',
+    entry: './ex3/index.jsx',
     output: {
         path: __dirname + '/public',
         filename: 'bundle.js'
