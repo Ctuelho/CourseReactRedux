44900f3f671ef3a65634d963ed53522babcb6a3c
Túlio Duarte <duartulio@gmail.com>
2018-07-18 11:41:40 -0300

    last lesson

--- exercicios_react/ex2/index.jsx
+++ exercicios_react/ex2/index.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Component from './component.jsx'
 
-ReactDOM.render(Component, document.getElementById('app'))
+ReactDOM.render(<Component/>, document.getElementById('app'))
 
